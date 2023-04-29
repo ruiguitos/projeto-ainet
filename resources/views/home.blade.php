8c38bf5291bf143f0aa651b4c0cee085bffe7817
dazzlo123 <pedrolopes-two@hotmail.com>
2023-04-29 16:31:55 +0100

    updated homepage

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,5 +4,40 @@
     @include('partials.sidebar')
 @endsection
 @section('content')
-    <div>Bem-vindos à Loja ImagineShirt.</div>
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Magic T-Shirts</title>
+	<link rel="stylesheet" type="text/css" href="/resources/css/app.css">
+</head>
+<body>
+	<header>
+		<div class="container">
+			<h1>Online Shop</h1>
+			<nav>
+				<ul>
+					<li><a href="#">Home</a></li>
+					<li><a href="#">Produtos</a></li>
+					<li><a href="#">Acerca de Nós</a></li>
+					<li><a href="#">Contactos</a></li>
+				</ul>
+			</nav>
+		</div>
+	</header>
+
+	<section>
+		<div class="container">
+			<h2>Bem vindo a nossa loja </h2>
+			<p>Temos bueda merdas</p>
+			<a href="#" class="button">Compra AQUI BURRO</a>
+		</div>
+	</section>
+
+	<footer>
+		<div class="container">
+			
+		</div>
+	</footer>
+</body>
+</html>
 @endsection
