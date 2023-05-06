bd1a17832fba4fb0036b64bd5f5407abd2d5bd7b
ruiguitos <ruiguitos@hotmail.com>
2023-05-06 22:49:12 +0100

    now properly showing user avatar according to each user logged in

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -1,12 +1,12 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 
 <head>
-    <meta charset="utf-8" />
-    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
-    <meta name="description" content="" />
-    <meta name="author" content="" />
+    <meta charset="utf-8"/>
+    <meta http-equiv="X-UA-Compatible" content="IE=edge"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no"/>
+    <meta name="description" content=""/>
+    <meta name="author" content=""/>
     <title>ImagineShirt</title>
     @vite('resources/sass/app.scss')
     <script src="https://use.fontawesome.com/releases/v6.1.0/js/all.js" crossorigin="anonymous"></script>
@@ -19,9 +19,13 @@
         <img src="/img/plain_white.png" alt="Logo" class="bg-dark" width="50" height="50">
         ImagineShirt
     </a>
+
     <!-- Sidebar Toggle-->
-    <button class="btn btn-link btn-sm order-1 order-lg-0 me-3 me-lg-0" id="sidebarToggle" href="#"><i
-            class="fas fa-bars"></i></button>
+    <button class="btn btn-link btn-sm order-1 order-lg-0 me-3 me-lg-0" id="sidebarToggle" href="#">
+        <i
+            class="fas fa-bars">
+        </i>
+    </button>
     @guest
         <ul class="navbar-nav ms-auto me-1 me-lg-3">
             @if (Route::has('login'))
@@ -48,15 +52,17 @@
             <li class="nav-item dropdown">
                 <a class="nav-link dropdown-toggle" id="navbarDropdown" href="#" role="button"
                    data-bs-toggle="dropdown" aria-expanded="false">
-                    <img src="/img/avatar_unknown.png" alt="Avatar" class="bg-dark rounded-circle" width="45"
-                         height="45">
+                    <img
+                        src="{{Auth::user()->photo_url ? asset('storage/photos/' . Auth::user()->photo_url) : asset('img/default_img.png') }}"
+                        alt="Imagem do Cliente" class="img-profile rounded-circle" height="45">
+
                 </a>
 
                 <ul class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
-                    <li><a class="dropdown-item" href="#">Perfil</a></li>
-                    <li><a class="dropdown-item" href="#">Alterar Senha</a></li>
+                    <li><a class="dropdown-item" href="/perfil">Perfil</a></li>
+                    <li><a class="dropdown-item" href="/password/reset">Alterar Senha</a></li>
                     <li>
-                        <hr class="dropdown-divider" />
+                        <hr class="dropdown-divider"/>
                     </li>
                     <li>
                         <a class="dropdown-item"
@@ -78,62 +84,25 @@
         <nav class="sb-sidenav accordion sb-sidenav-dark" id="sidenavAccordion">
             <div class="sb-sidenav-menu">
                 <div class="nav">
-                    <a class="nav-link" href="#">
+                    <a class="nav-link" href="/dashboard">
                         <div class="sb-nav-link-icon"><i class="fas fa-home"></i></div>
-                        Dashboard
+                        Admin Dashboard
                     </a>
-                    <a class="nav-link" href="#">
+
+                    <a class="nav-link" href="/catalogo">
                         <div class="sb-nav-link-icon"><i class="fas fa-table"></i></div>
-                        Planos Curriculares
+                        Catálogo de T-Shirts
                     </a>
+
                     <a class="nav-link" href="#">
                         <div class="sb-nav-link-icon"><i class="fas fa-graduation-cap"></i></div>
-                        Alunos
+                        Catálogo de Imagens
                     </a>
-                    <div class="sb-sidenav-menu-heading">Gestão</div>
-                    <a class="nav-link collapsed" href="#" data-bs-toggle="collapse"
-                       data-bs-target="#collapseCurricular" aria-expanded="false"
-                       aria-controls="collapseCurricular">
-                        <div class="sb-nav-link-icon"><i class="fas fa-columns"></i></div>
-                        Curricular
-                        <div class="sb-sidenav-collapse-arrow"><i class="fas fa-angle-down"></i></div>
-                    </a>
-{{--                    <div class="collapse" id="collapseCurricular" aria-labelledby="headingOne"--}}
-{{--                         data-bs-parent="#sidenavAccordion">--}}
-{{--                        <nav class="sb-sidenav-menu-nested nav">--}}
-{{--                            <a class="nav-link {{ Route::currentRouteName() == 'cursos.index' ? 'active' : '' }}"--}}
-{{--                               href="{{ route('cursos.index') }}">Cursos</a>--}}
-{{--                            <a class="nav-link {{ Route::currentRouteName() == 'disciplinas.index' ? 'active' : '' }}"--}}
-{{--                               href="{{ route('disciplinas.index') }}">Disciplinas</a>--}}
-{{--                        </nav>--}}
-{{--                    </div>--}}
-                    <a class="nav-link collapsed" href="#" data-bs-toggle="collapse"
-                       data-bs-target="#collapseRecursosHumanos" aria-expanded="false"
-                       aria-controls="collapseRecursosHumanos">
-                        <div class="sb-nav-link-icon"><i class="fas fa-user"></i></div>
-                        Recursos Humanos
-                        <div class="sb-sidenav-collapse-arrow"><i class="fas fa-angle-down"></i></div>
-                    </a>
-                    <div class="collapse" id="collapseRecursosHumanos" aria-labelledby="headingTwo"
-                         data-bs-parent="#sidenavAccordion">
-                        <nav class="sb-sidenav-menu-nested nav">
-                            <a class="nav-link" href="#">Departamentos</a>
-                            <a class="nav-link" href="#">Docentes</a>
-                        </nav>
-                    </div>
-                    <div class="sb-sidenav-menu-heading">Candidaturas</div>
-                    <a class="nav-link" href="#">
-                        <div class="sb-nav-link-icon"><i class="fas fa-upload"></i></div>
-                        Enviar candidatura
-                    </a>
-                    <a class="nav-link" href="#">
-                        <div class="sb-nav-link-icon"><i class="fas fa-envelope"></i></div>
-                        Candidaturas
-                    </a>
+
                     <div class="sb-sidenav-menu-heading">Espaço Privado</div>
                     <a class="nav-link" href="#">
                         <div class="sb-nav-link-icon"><i class="fas fa-file-text"></i></div>
-                        Minhas disciplinas
+                       ddasdasdasdasdasdas
                     </a>
                 </div>
             </div>
