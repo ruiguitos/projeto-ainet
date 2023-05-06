7e391f527c0eb247463eaba9493e031e3af9ba75
ruiguitos <ruiguitos@hotmail.com>
2023-05-06 22:48:31 +0100

    created routes for "catalogo", "admin dashboard" and "perfil"

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,13 @@
 
 use Illuminate\Support\Facades\Route;
 
+use App\Http\Controllers\CustomerController;
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\DashboardController;
+
+
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -20,4 +27,23 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+//CATALOGO
+Route::view('/catalogo', 'catalogo.index');
+
+//ADMIN DASHBOARD
+Route::view('/dashboard', 'dashboard.index');
+Route::view('/dashboard/charts', 'dashboard.charts');
+Route::view('/dashboard/tables', 'dashboard.tables');
+
+//PERFIL
+Route::view('/perfil', 'perfil.index');
+Route::get('/perfil/{customer}', [CustomerController::class, 'index'])->name('perfil.index');
+Route::get('/perfil/{customer}/edit', [CustomerController::class, 'edit'])->name('perfil.edit');
+Route::put('/perfil/{customer}', [CustomerController::class, 'update'])->name('perfil.update');
+Route::delete('/perfil/{customer}', [CustomerController::class, 'destroy'])->name('perfil.destroy');
+Route::delete('/perfil/{customer}/photo', [CustomerController::class, 'destroy_photo'])->name('perfil.photo.destroy');
+
+//CHANGE PASSWORD
+Route::view('/password/reset', 'auth.passwords.reset');
 
