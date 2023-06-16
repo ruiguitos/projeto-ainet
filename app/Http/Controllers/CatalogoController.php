40c4c5ff4e36ad6c6b99e8011b0299a812c66bff
ruiguitos <ruiguitos@hotmail.com>
2023-06-17 00:39:33 +0100

    created Catalogo controller

---
+++ app/Http/Controllers/CatalogoController.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Preco;
+use App\Models\TShirt;
+use App\Models\Categoria;
+use Illuminate\Http\Request;
+
+class CatalogoController extends Controller
+{
+    public function index()
+    {
+        $tshirt_images = TShirt::select('id', 'name', 'category_id', 'description', 'image_url')->paginate(20);
+        $prices = Preco::all();
+        return view('catalogo.index', compact('tshirt_images', 'prices'));
+    }
+
+    public function show($id)
+    {
+        $tshirt_images = TShirt::findOrFail($id);
+        $prices = Preco::all();
+        $categories = Categoria::all();
+        return view('catalogo.show', compact('tshirt_images', 'prices', 'categories'));
+    }
+
+//    public function filter(Request $request)
+//    {
+//        $categories = Categoria::all();
+//        $query = Categoria::query();
+//
+//        // Filter by category
+//        if ($request->has('category')) {
+//            $category = $request->input('category');
+//            $query->where('category_id', $category);
+//        }
+//
+//        // Sort by name
+//        if ($request->has('sort')) {
+//            $sort = $request->input('sort');
+//            $query->orderBy('name', $sort);
+//        }
+//
+//        // Search by name
+//        if ($request->has('search')) {
+//            $search = $request->input('search');
+//            $query->where('name', 'like', "%$search%");
+//        }
+//
+//        $tshirts = $query->get();
+//
+//        return view('catalog.index', compact('tshirts', 'categories'));
+//    }
+
+}
