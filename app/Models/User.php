29cc16eeece083ea683de6beb764e88e6fadad88
ruiguitos <ruiguitos@hotmail.com>
2023-06-17 12:08:56 +0100

    updated user model

--- app/Models/User.php
+++ app/Models/User.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\HasOne;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
@@ -11,6 +12,8 @@ use Laravel\Sanctum\HasApiTokens;
 class User extends Authenticatable
 {
     use HasApiTokens, HasFactory, Notifiable;
+
+    protected $table = 'users';
 
     /**
      * The attributes that are mass assignable.
@@ -21,12 +24,9 @@ class User extends Authenticatable
         'name',
         'email',
         'password',
+        'user_type',
         'blocked',
-        'photo_url',
-        'address',
-        'nif',
-        'payment_type',
-        'payment_ref'
+        'photo_url'
     ];
 
     /**
@@ -49,5 +49,9 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function customer(): HasOne{
+        return $this->hasOne(Cliente::class, 'id','id');
+    }
 
 }
