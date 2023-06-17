3a6716cb941100122f7eabc2f0a66d873251a97b
ruiguitos <ruiguitos@hotmail.com>
2023-06-17 12:06:52 +0100

    updated perfil view

--- resources/views/perfil/index.blade.php
+++ resources/views/perfil/index.blade.php
@@ -31,29 +31,45 @@
     <table class="table">
         <tr>
             <th>NIF</th>
-            {{--            <td>{{ Auth::user()->nif }}</td>--}}
+            <td>{{ Auth::user()->nif }}</td>
         </tr>
         <tr>
             <th>Endereço</th>
-            {{--            <td>{{Auth::user()->address}}</td>--}}
+            <td>{{Auth::user()->address}}</td>
         </tr>
         <tr>
             <th>Tipo de Pagamento</th>
-        {{--                        @if(Auth::user()->customers->ref_type == 'MC')--}}
-        {{--                            <td>Master Card</td>--}}
-        {{--                        @elseif(Auth::user()->customers->ref_type == 'PAYPAL')--}}
-        {{--                            <td>Pay Pal</td>--}}
-        {{--                        @elseif(Auth::user()->customers->ref_type == 'VISA')--}}
-        {{--                            <td>Visa</td>--}}
-        {{--                        @else--}}
-        {{--                            <td></td>--}}
-        {{--                        @endif--}}
-        {{--        </tr>--}}
+            <td>
+                @if (Auth::user()->customers)
+                    @if (Auth::user()->customers->ref_type == 'MC')
+                        Master Card
+                    @elseif (Auth::user()->customers->ref_type == 'PAYPAL')
+                        Pay Pal
+                    @elseif (Auth::user()->customers->ref_type == 'VISA')
+                        Visa
+                    @endif
+                @endif
+            </td>
+        </tr>
         <tr>
             <th>Referência de Pagamento</th>
-            {{--            <td>{{Auth::user()->customers->payment_ref}}</td>--}}
+            <td>
+                @if (Auth::user()->customers)
+                    {{ Auth::user()->customers->payment_ref }}
+                @endif
+            </td>
         </tr>
     </table>
+
+    <!-- Edit Profile button -->
+    <center>
+        <a href="{{ route('perfil.edit', Auth::user()->id) }}" class="btn btn-primary btn-sm" role="button"
+           aria-pressed="true" style="border-color: black">
+            Editar Perfil
+        </a>
+    </center>
+    <br>
+
     <center>
         {{--        <a href="{{ route('perfil.edit', Auth::user()->customers->id) }}" class="btn btn-primary btn-sm"--}}
         {{--           role="button" aria-pressed="true" style="border-color: black">Alterar Perfil</a>--}}
