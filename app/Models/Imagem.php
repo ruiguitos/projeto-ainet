aaf49d313bd826a94578cace619a6950fef5c15f
ruiguitos <ruiguitos@hotmail.com>
2023-06-17 12:08:37 +0100

    updated imagem model

--- app/Models/Imagem.php
+++ app/Models/Imagem.php
@@ -1,1 +1,36 @@
 <?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+
+
+class Imagem extends Model
+{
+
+    use HasFactory, SoftDeletes;
+
+    protected $table = 'tshirt_images';
+
+    protected $fillable = ['customer_id', 'category_id', 'name', 'description', 'image_url', 'extra_info'];
+
+    public function tshirt(): HasMany
+    {
+        return $this->hasMany(Imagem::Class);
+    }
+
+    public function costumerId(): BelongsTo
+    {
+        return $this->belongsTo(Cliente::Class, 'customer_id', 'id');
+    }
+
+    public function categoryId() : BelongsTo
+    {
+        return $this->belongsTo(Categoria::Class, 'category_id', 'id');
+    }
+}
