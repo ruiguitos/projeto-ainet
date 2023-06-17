b1bd48750c78701a3b92b401ad16f930e9c04475
ruiguitos <ruiguitos@hotmail.com>
2023-06-17 10:33:33 +0100

    add categorias view (to be continued)

---
+++ resources/views/categorias/index.blade.php
@@ -0,0 +1,9 @@
+@extends('layout')
+
+@section('main')
+
+    <br>
+    <center><a href="{{ route('home')}}" class="btn btn-default" style="border-color: black">Voltar à Pagina Inicial</a>
+    </center>
+
+@endsection
