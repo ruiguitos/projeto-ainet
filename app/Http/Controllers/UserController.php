814c3fa6bbbde0927f9d9ea8d5e0f4733ad4050c
ruiguitos <ruiguitos@hotmail.com>
2023-06-19 19:39:21 +0100

    add functions CRUD functions

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\CorPost;
 use Illuminate\Http\Request;
 use App\Models\User;
 use App\Http\Requests\UserPost;
@@ -59,5 +60,69 @@ class UserController extends Controller
         // Redirect or return response as needed
         return redirect()->back();
     }
+
+    public function edit(User $User)
+    {
+        return view('users.admins.edit')
+            ->withAdmins($User)
+            ->withClientes($User)
+            ->withEmpregados($User);
+    }
+
+    public function create(User $User)
+    {
+        return view('users.admins.create')
+            ->withAdmins($User)
+            ->withClientes($User)
+            ->withEmpregados($User);
+    }
+
+    public function store(UserPost $request)
+    {
+        $validated_data = $request->validated();
+        User::create($validated_data);
+        return redirect()->route('users.admins.index')
+            ->with('alert-msg', 'Utilizador(a) "' . $validated_data['nome'] . '" foi criado(a) com sucesso!')
+            ->with('alert-type', 'success');
+    }
+
+    public function update(UserPost $request, User $User)
+    {
+        $validated_data = $request->validated();
+        $User->fill($validated_data);
+        $User->save();
+        return redirect()->route('users.admins.index')
+            ->with('alert-msg', 'Utilizador(a) "' . $User->name . '" foi alterado(a) com sucesso!')
+            ->with('alert-type', 'success');
+    }
+
+    public function destroy(User $User)
+    {
+        $oldName = $User->nome;
+        try {
+            $User->delete();
+            return redirect()->route('users.admins.index')
+                ->with('alert-msg', 'Utilizador(a) "' . $User->name . '" foi apagado(a) com sucesso!')
+                ->with('alert-type', 'success');
+        } catch (\Throwable $th) {
+            // $th é a exceção lançada pelo sistema - por norma, erro ocorre no servidor BD MySQL
+            // Descomentar a próxima linha para verificar qual a informação que a exceção tem
+            //dd($th, $th->errorInfo);
+
+            if ($th->errorInfo[1] == 1451) {   // 1451 - MySQL Error number for "Cannot delete or update a parent row: a foreign key constraint fails (%s)"
+                return redirect()->route('users.admins.admin')
+                    ->with('alert-msg', 'Não foi possível apagar o/a utilizador(a)"' . $oldName . '", porque está em uso!')
+                    ->with('alert-type', 'danger');
+            } else {
+                return redirect()->route('users.admins.admin')
+                    ->with('alert-msg', 'Não foi possível apagar o/a utilizador(a)"' . $oldName . '". Erro: ' . $th->errorInfo[2])
+                    ->with('alert-type', 'danger');
+            }
+        }
+    }
+
+
+
+
 
 }
