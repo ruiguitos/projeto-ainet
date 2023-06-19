f26ca36aa3bf9e727ce5b7b02c00c8e18efe29f6
ruiguitos <ruiguitos@hotmail.com>
2023-06-19 19:38:18 +0100

    updated user pages

--- resources/views/users/admins/index.blade.php
+++ resources/views/users/admins/index.blade.php
@@ -2,12 +2,10 @@
 {{--@section('title','Cor' )--}}
 @section('main')
 
-    <div class="container-fluid px-4">
-        <h1 class="mt-4">@yield('titulo', 'Administradores')</h1>
-        @yield('subtitulo')
-        <div class="mt-4">
-            @yield('main')
-        </div>
+    <h1 class="mt-4">@yield('titulo', 'Administradores')</h1>
+    @yield('subtitulo')
+    <div class="mt-4">
+        @yield('main')
     </div>
 
     <table class="table table-sm">
@@ -18,7 +16,6 @@
             <th>Email</th>
             <th>User type</th>
             <th>Estado</th>
-            <th>Actions </th>
         </tr>
         </thead>
         <tbody>
@@ -30,18 +27,36 @@
                     <td>{{ $user->name }}</td>
                     <td>{{ $user->email }}</td>
                     <td>{{ $user->user_type }}</td>
-                    @if($user->blocked == 1)
-                        <td> Bloqueado </td>
-                    @else
-                        <td> Ativo </td>
-                    @endif
+{{--                    @if($user->blocked == 1)--}}
+{{--                        <td> Bloqueado </td>--}}
+{{--                    @else--}}
+{{--                        <td> Ativo </td>--}}
+{{--                    @endif--}}
                     <td>
                         <form id="toggleForm" action="{{ route('users.admins.index', ['id' => $user->id]) }}" method="POST" style="display: inline;">
                             @csrf
                             @method('PUT')
-                            <button type="submit" class="btn {{ $user->blocked ? 'btn-success' : 'btn-danger' }}">
-                                {{ $user->blocked ? 'Unblock' : 'Block' }}
+                            <button type="submit" class="btn {{ $user->blocked ? 'btn-info' : 'btn-warning' }}">
+                                {{ $user->blocked ? 'Desativado' : 'Ativado' }}
                             </button>
+                        </form>
+                    </td>
+                    <td>
+                        <a href="{{route('users.admins.edit', ['admin' => $user]) }}" class="btn btn-primary btn-sm" role="button" aria-pressed="true">Alterar</a>
+                    <td>
+                        @can('delete', $user)
+                            <form action="{{route('users.admins.destroy', ['admin' => $user]) }}" method="POST">
+                                @csrf
+                                @method("DELETE")
+                                <input type="submit" class="btn btn-danger btn-sm" value="Apagar">
+                            </form>
+                        @endcan
+                    </td>
+                    <td>
+                        <form action="{{route('users.admins.destroy', ['admin' => $user]) }}" method="POST">
+                            @csrf
+                            @method("DELETE")
+                            <input type="submit" class="btn btn-danger btn-sm" value="Apagar">
                         </form>
                     </td>
                 </tr>
@@ -49,25 +64,113 @@
         @endforeach
         </tbody>
     </table>
+
+    <div id="layoutSidenav_content">
+        <main>
+            <div class="container-fluid px-4">
+                <br>
+                @yield('subtitulo')
+                <div class="mt-4">
+                    @yield('main')
+                    <div id="layoutSidenav">
+                        <div id="layoutSidenav_nav">
+                            <nav class="sb-sidenav accordion sb-sidenav-dark" id="sidenavAccordion">
+                                <div class="sb-sidenav-menu">
+                                    <div class="nav">
+                                        <div class="sb-sidenav-menu-heading">Core</div>
+                                        <a class="nav-link" href="/home">
+                                            <div class="sb-nav-link-icon"><i class="fa-solid fa-house-chimney"></i>
+                                            </div>
+                                            Homepage
+                                        </a>
+
+                                        <div class="sb-sidenav-menu-heading">Área de Gestão</div>
+                                        <a class="nav-link" href="/categoria">
+                                            <div class="sb-nav-link-icon">
+                                                <i class="fa-solid fa-list"></i>
+                                            </div>
+                                            Categoria Disponíveis
+                                        </a>
+
+                                        <a class="nav-link" href="/cores">
+                                            <div class="sb-nav-link-icon">
+                                                <i class="fa-solid fa-list"></i>
+                                            </div>
+                                            Cores
+                                        </a>
+
+                                        <a class="nav-link" href="/encomendas">
+                                            <div class="sb-nav-link-icon">
+                                                <i class="fa-solid fa-list"></i>
+                                            </div>
+                                            Encomendas Registadas
+                                        </a>
+
+                                        <a class="nav-link" href="/encomendas">
+                                            <div class="sb-nav-link-icon">
+                                                <i class="fa-solid fa-list"></i>
+                                            </div>
+                                            Status Encomendas
+                                        </a>
+
+                                        <a class="nav-link" href="/dashboard/charts">
+                                            <div class="sb-nav-link-icon"><i class="fas fa-chart-area"></i></div>
+                                            Gráficos
+                                        </a>
+                                        <a class="nav-link" href="/dashboard/tables">
+                                            <div class="sb-nav-link-icon"><i class="fas fa-table"></i></div>
+                                            Dados
+                                        </a>
+
+                                        <div class="sb-sidenav-menu-heading">Utilizadores</div>
+                                        <a class="nav-link" href="/users/admins">
+                                            <div class="sb-nav-link-icon"><i class="fas fa-users"></i>
+                                            </div>
+                                            Administradores
+                                        </a>
+
+                                        <a class="nav-link" href="/users/clientes">
+                                            <div class="sb-nav-link-icon"><i class="fas fa fa-users"></i></div>
+                                            Clientes
+                                        </a>
+
+                                        <a class="nav-link" href="/users/empregados">
+                                            <div class="sb-nav-link-icon"><i class="fas fa fa-users"></i></div>
+                                            Empregados
+                                        </a>
+                                    </div>
+                                </div>
+                                <div class="sb-sidenav-footer">
+                                    <div class="small">
+                                        <strong>Logged in as:</strong>
+                                    </div>
+                                    @php
+                                        $fullName = Auth::user()->name;
+                                        $nameParts = explode(' ', $fullName);
+                                        $firstName = $nameParts[0];
+                                        $lastName = count($nameParts) > 1 ? $nameParts[count($nameParts) - 1] : '';
+                                    @endphp
+                                    {{ $firstName }} {{ $lastName }}
+                                    <div class="small" style="margin-top: 10px">
+                                        <strong>User type:</strong>
+                                        @php
+                                            $userType = Auth::user()->user_type;
+                                            $userTypeMeanings = [
+                                                'A' => 'Administrador',
+                                                'E' => 'Empregado',
+                                            ];
+                                        @endphp
+                                        {{ isset($userTypeMeanings[$userType]) ? $userTypeMeanings[$userType] : 'Unknown' }}
+                                    </div>
+                                </div>
+                            </nav>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+    <footer>
+        {{ $users->links() }}
+    </footer>
+
 @endsection
-
-<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
-<script>
-    $(document).ready(function() {
-        $('#toggleForm').submit(function(e) {
-            e.preventDefault();
-
-            $.ajax({
-                type: 'POST',
-                url: $(this).attr('action'),
-                data: $(this).serialize(),
-                success: function(response) {
-                    location.reload();
-                },
-                error: function(response) {
-                    console.log(response);
-                }
-            });
-        });
-    });
-</script>
--- resources/views/users/clientes/index.blade.php
+++ resources/views/users/clientes/index.blade.php
@@ -1,14 +1,11 @@
 @extends('layout')
-{{--@section('title','Cor')--}}
 
 @section('main')
 
-    <div class="container-fluid px-4">
-        <h1 class="mt-4">@yield('titulo', 'Clientes')</h1>
-        @yield('subtitulo')
-        <div class="mt-4">
-            @yield('main')
-        </div>
+    <h1 class="mt-4">@yield('titulo', 'Clientes')</h1>
+    @yield('subtitulo')
+    <div class="mt-4">
+        @yield('main')
     </div>
 
     <ol class="breadcrumb">
@@ -33,17 +30,17 @@
                 <td>{{ $user->id }}</td>
                 <td>{{ $user->name }}</td>
                 <td>{{ $user->email }}</td>
-                @if($user->blocked == 1)
-                    <td> Bloqueado </td>
-                @else
-                    <td> Ativo </td>
-                @endif
+{{--                @if($user->blocked == 1)--}}
+{{--                    <td> Bloqueado </td>--}}
+{{--                @else--}}
+{{--                    <td> Ativo </td>--}}
+{{--                @endif--}}
                 <td>
                     <form id="toggleForm" action="{{ route('users.clientes.index', ['id' => $user->id]) }}" method="POST" style="display: inline;">
                         @csrf
                         @method('PUT')
-                        <button type="submit" class="btn {{ $user->blocked ? 'btn-success' : 'btn-danger' }}">
-                            {{ $user->blocked ? 'Unblock' : 'Block' }}
+                        <button type="submit" class="btn {{ $user->blocked ? 'btn-info' : 'btn-warning' }}">
+                            {{ $user->blocked ? 'Desativado' : 'Ativado' }}
                         </button>
                     </form>
                 </td>
@@ -60,7 +57,7 @@
                 </td>
                 <td>
                     <form action="{{route('users.clientes.destroy', ['cliente' => $user]) }}" method="POST">
-                        @crsf
+                        @csrf
                         @method("DELETE")
                         <input type="submit" class="btn btn-danger btn-sm" value="Apagar">
                     </form>
@@ -71,6 +68,111 @@
         </tbody>
     </table>
 
-    <footer>{{ $users->links() }}</footer>
+    <div id="layoutSidenav_content">
+        <main>
+            <div class="container-fluid px-4">
+                <br>
+                @yield('subtitulo')
+                <div class="mt-4">
+                    @yield('main')
+                    <div id="layoutSidenav">
+                        <div id="layoutSidenav_nav">
+                            <nav class="sb-sidenav accordion sb-sidenav-dark" id="sidenavAccordion">
+                                <div class="sb-sidenav-menu">
+                                    <div class="nav">
+                                        <div class="sb-sidenav-menu-heading">Core</div>
+                                        <a class="nav-link" href="/home">
+                                            <div class="sb-nav-link-icon"><i class="fa-solid fa-house-chimney"></i>
+                                            </div>
+                                            Homepage
+                                        </a>
 
+                                        <div class="sb-sidenav-menu-heading">Área de Gestão</div>
+                                        <a class="nav-link" href="/categoria">
+                                            <div class="sb-nav-link-icon">
+                                                <i class="fa-solid fa-list"></i>
+                                            </div>
+                                            Categoria Disponíveis
+                                        </a>
+
+                                        <a class="nav-link" href="/cores">
+                                            <div class="sb-nav-link-icon">
+                                                <i class="fa-solid fa-list"></i>
+                                            </div>
+                                            Cores
+                                        </a>
+
+                                        <a class="nav-link" href="/encomendas">
+                                            <div class="sb-nav-link-icon">
+                                                <i class="fa-solid fa-list"></i>
+                                            </div>
+                                            Encomendas Registadas
+                                        </a>
+
+                                        <a class="nav-link" href="/encomendas">
+                                            <div class="sb-nav-link-icon">
+                                                <i class="fa-solid fa-list"></i>
+                                            </div>
+                                            Status Encomendas
+                                        </a>
+
+                                        <a class="nav-link" href="/dashboard/charts">
+                                            <div class="sb-nav-link-icon"><i class="fas fa-chart-area"></i></div>
+                                            Gráficos
+                                        </a>
+                                        <a class="nav-link" href="/dashboard/tables">
+                                            <div class="sb-nav-link-icon"><i class="fas fa-table"></i></div>
+                                            Dados
+                                        </a>
+
+                                        <div class="sb-sidenav-menu-heading">Utilizadores</div>
+                                        <a class="nav-link" href="/users/admins">
+                                            <div class="sb-nav-link-icon"><i class="fas fa-users"></i>
+                                            </div>
+                                            Administradores
+                                        </a>
+
+                                        <a class="nav-link" href="/users/clientes">
+                                            <div class="sb-nav-link-icon"><i class="fas fa fa-users"></i></div>
+                                            Clientes
+                                        </a>
+
+                                        <a class="nav-link" href="/users/empregados">
+                                            <div class="sb-nav-link-icon"><i class="fas fa fa-users"></i></div>
+                                            Empregados
+                                        </a>
+                                    </div>
+                                </div>
+                                <div class="sb-sidenav-footer">
+                                    <div class="small">
+                                        <strong>Logged in as:</strong>
+                                    </div>
+                                    @php
+                                        $fullName = Auth::user()->name;
+                                        $nameParts = explode(' ', $fullName);
+                                        $firstName = $nameParts[0];
+                                        $lastName = count($nameParts) > 1 ? $nameParts[count($nameParts) - 1] : '';
+                                    @endphp
+                                    {{ $firstName }} {{ $lastName }}
+                                    <div class="small" style="margin-top: 10px">
+                                        <strong>User type:</strong>
+                                        @php
+                                            $userType = Auth::user()->user_type;
+                                            $userTypeMeanings = [
+                                                'A' => 'Administrador',
+                                                'E' => 'Empregado',
+                                            ];
+                                        @endphp
+                                        {{ isset($userTypeMeanings[$userType]) ? $userTypeMeanings[$userType] : 'Unknown' }}
+                                    </div>
+                                </div>
+                            </nav>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+    <footer>
+        {{ $users->links() }}
+    </footer>
 @endsection
