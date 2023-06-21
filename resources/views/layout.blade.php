b018b727fb4cc6f5bfd5517f0b07cef686f4a731
ruiguitos <ruiguitos@hotmail.com>
2023-06-21 21:48:46 +0100

    admins can see all the views && clients can only see some menus

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -1,5 +1,159 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+{{--<!DOCTYPE html>--}}
+{{--<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">--}}
+
+{{--<head>--}}
+{{--    <meta charset="utf-8"/>--}}
+{{--    <meta http-equiv="X-UA-Compatible" content="IE=edge"/>--}}
+{{--    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no"/>--}}
+{{--    <meta name="description" content=""/>--}}
+{{--    <meta name="author" content=""/>--}}
+{{--    <title>ImagineShirt</title>--}}
+{{--    <link rel="icon" type="image/x-icon" href="/img/plain_white.png">--}}
+{{--    @vite('resources/sass/app.scss')--}}
+{{--    <script src="https://use.fontawesome.com/releases/v6.1.0/js/all.js" crossorigin="anonymous"></script>--}}
+{{--</head>--}}
+
+{{--<body class="sb-nav-fixed">--}}
+{{--<nav class="sb-topnav navbar navbar-expand navbar-dark bg-dark">--}}
+{{--    <!-- Navbar Brand-->--}}
+{{--    <a class="navbar-brand " href="{{ route('home') }}">--}}
+{{--        <img src="/img/plain_white.png" alt="Logo" class="bg-dark" width="50" height="50">--}}
+{{--        ImagineShirt--}}
+{{--    </a>--}}
+
+{{--    <!-- Sidebar Toggle-->--}}
+{{--    <button class="btn btn-link btn-sm order-1 order-lg-0 me-3 me-lg-0" id="sidebarToggle" href="#">--}}
+{{--        <i--}}
+{{--            class="fas fa-bars">--}}
+{{--        </i>--}}
+{{--    </button>--}}
+{{--    @guest--}}
+{{--        <ul class="navbar-nav ms-auto me-1 me-lg-3">--}}
+{{--            @if (Route::has('login'))--}}
+{{--                <li class="nav-item">--}}
+{{--                    <a class="nav-link" href="{{ route('login') }}">--}}
+{{--                        {{ __('Login') }}--}}
+{{--                    </a>--}}
+{{--                </li>--}}
+{{--            @endif--}}
+{{--            @if (Route::has('register'))--}}
+{{--                <li class="nav-item">--}}
+{{--                    <a class="nav-link" href="{{ route('register') }}">--}}
+{{--                        {{ __('Register') }}--}}
+{{--                    </a>--}}
+{{--                </li>--}}
+{{--            @endif--}}
+{{--        </ul>--}}
+{{--    @else--}}
+{{--        <div class="ms-auto me-0 me-md-2 my-2 my-md-0 navbar-text">--}}
+{{--            {{ Auth::user()->name }}--}}
+{{--        </div>--}}
+{{--        <!-- Navbar-->--}}
+{{--        <ul class="navbar-nav me-1 me-lg-3">--}}
+{{--            <li class="nav-item dropdown">--}}
+{{--                <a class="nav-link dropdown-toggle" id="navbarDropdown" href="#" role="button"--}}
+{{--                   data-bs-toggle="dropdown" aria-expanded="false">--}}
+
+{{--                --}}{{-- Adiciona foto de utilizador e verifica se tem foto ou nao. se tiver mete a foto, senão mete uma default --}}
+{{--                    @if(Auth::user()->photo_url)--}}
+{{--                        <img src="{{ asset('storage/photos/' . Auth::user()->photo_url) }}"--}}
+{{--                             class="img-profile rounded-circle" height="45">--}}
+{{--                    @else--}}
+{{--                        <img src="{{ asset('img/default_img.png') }}" class="img-profile rounded-circle" height="45">--}}
+{{--                    @endif--}}
+{{--                </a>--}}
+
+{{--                <ul class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">--}}
+{{--                    <li>--}}
+{{--                        <a class="dropdown-item" href="/perfil">--}}
+{{--                            Perfil--}}
+{{--                        </a>--}}
+{{--                    </li>--}}
+{{--                    <li>--}}
+{{--                        <a class="dropdown-item" href="/password/reset">--}}
+{{--                            Alterar Senha--}}
+{{--                        </a>--}}
+{{--                    </li>--}}
+{{--                    <li>--}}
+{{--                        <a class="dropdown-item" href="/carrinho">--}}
+{{--                            Carrinho--}}
+{{--                        </a>--}}
+{{--                    </li>--}}
+{{--                    <li>--}}
+{{--                        <hr class="dropdown-divider"/>--}}
+{{--                    </li>--}}
+{{--                    <li>--}}
+{{--                        <a class="dropdown-item"--}}
+{{--                           onclick="event.preventDefault();--}}
+{{--                                        document.getElementById('logout-form').submit();">--}}
+{{--                            Sair--}}
+{{--                        </a>--}}
+{{--                        <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">--}}
+{{--                            @csrf--}}
+{{--                        </form>--}}
+{{--                    </li>--}}
+{{--                </ul>--}}
+{{--            </li>--}}
+{{--        </ul>--}}
+{{--    @endguest--}}
+{{--</nav>--}}
+{{--<div id="layoutSidenav">--}}
+{{--    <div id="layoutSidenav_nav">--}}
+{{--        <nav class="sb-sidenav accordion sb-sidenav-dark" id="sidenavAccordion">--}}
+{{--            <div class="sb-sidenav-menu">--}}
+{{--                <div class="nav">--}}
+{{--                    <div class="sb-sidenav-menu-heading">Espaço de Compras</div>--}}
+{{--                        <a class="nav-link" href="/catalogo">--}}
+{{--                            <div class="sb-nav-link-icon"><i class="fas fa-table"></i></div>--}}
+{{--                            Catálogo--}}
+{{--                        </a>--}}
+
+{{--                        <a class="nav-link" href="/camisolas">--}}
+{{--                            <div class="sb-nav-link-icon"><i class="fas fa-graduation-cap"></i></div>--}}
+{{--                            Camisolas--}}
+{{--                        </a>--}}
+
+{{--                    @if(Auth::user()->user_type === 'A') //CHANGE--}}
+{{--                        <div class="sb-sidenav-menu-heading">Espaço Admin</div>--}}
+{{--                        <a class="nav-link" href="/dashboard">--}}
+{{--                            <div class="sb-nav-link-icon"><i class="fa fa-tachometer" aria-hidden="true"></i></div>--}}
+{{--                            Admin Dashboard--}}
+{{--                        </a>--}}
+{{--                    @endif--}}
+{{--                </div>--}}
+{{--            </div>--}}
+{{--            <div class="sb-sidenav-footer">--}}
+{{--                <div class="small">Logged in as: </div>--}}
+{{--                {{ Auth::user()->name }}--}}
+{{--            </div>--}}
+{{--        </nav>--}}
+{{--    </div>--}}
+{{--    <div id="layoutSidenav_content">--}}
+{{--        <main>--}}
+{{--            <div class="container-fluid px-4">--}}
+{{--                <br>--}}
+{{--                @yield('subtitulo')--}}
+{{--                <div class="mt-4">--}}
+{{--                    @yield('main')--}}
+{{--                </div>--}}
+{{--            </div>--}}
+{{--        </main>--}}
+{{--        <footer class="py-2 bg-light mt-auto">--}}
+{{--            <div class="container-fluid px-4">--}}
+{{--                <div class="d-flex align-items-center justify-content-between small">--}}
+{{--                    <div class="text-muted">Copyright &copy;ImagineShirt</div>--}}
+{{--                </div>--}}
+{{--            </div>--}}
+{{--        </footer>--}}
+{{--    </div>--}}
+{{--</div>--}}
+{{--@vite('resources/js/app.js')--}}
+{{--</body>--}}
+
+{{--</html>--}}
+
+    <!DOCTYPE html>
+<html lang="en">
 
 <head>
     <meta charset="utf-8"/>
@@ -8,7 +162,6 @@
     <meta name="description" content=""/>
     <meta name="author" content=""/>
     <title>ImagineShirt</title>
-    <link rel="icon" type="image/x-icon" href="/img/plain_white.png">
     @vite('resources/sass/app.scss')
     <script src="https://use.fontawesome.com/releases/v6.1.0/js/all.js" crossorigin="anonymous"></script>
 </head>
@@ -20,13 +173,9 @@
         <img src="/img/plain_white.png" alt="Logo" class="bg-dark" width="50" height="50">
         ImagineShirt
     </a>
-
     <!-- Sidebar Toggle-->
-    <button class="btn btn-link btn-sm order-1 order-lg-0 me-3 me-lg-0" id="sidebarToggle" href="#">
-        <i
-            class="fas fa-bars">
-        </i>
-    </button>
+    <button class="btn btn-link btn-sm order-1 order-lg-0 me-3 me-lg-0" id="sidebarToggle" href="#"><i
+            class="fas fa-bars"></i></button>
     @guest
         <ul class="navbar-nav ms-auto me-1 me-lg-3">
             @if (Route::has('login'))
@@ -53,7 +202,8 @@
             <li class="nav-item dropdown">
                 <a class="nav-link dropdown-toggle" id="navbarDropdown" href="#" role="button"
                    data-bs-toggle="dropdown" aria-expanded="false">
-                {{-- Adiciona foto de utilizador e verifica se tem foto ou nao. se tiver mete a foto, senão mete uma default --}}
+
+                    {{-- Adiciona foto de utilizador e verifica se tem foto ou nao. se tiver mete a foto, senão mete uma default --}}
                     @if(Auth::user()->photo_url)
                         <img src="{{ asset('storage/photos/' . Auth::user()->photo_url) }}"
                              class="img-profile rounded-circle" height="45">
@@ -63,15 +213,23 @@
                 </a>
 
                 <ul class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
-                    <li><a class="dropdown-item" href="/perfil">Perfil</a></li>
-                    <li><a class="dropdown-item" href="/password/reset">Alterar Senha</a></li>
-                    <li><a class="dropdown-item" href="/carrinho">Carrinho</a></li>
+                    <li>
+                        <a class="dropdown-item" href="/perfil">
+                            Perfil
+                        </a>
+                    </li>
+
+                    <li>
+                        <a class="dropdown-item" href="/carrinho">
+                            Carrinho
+                        </a>
+                    </li>
+                    <li><a class="dropdown-item" href="{{ route('password.change.show') }}">Alterar Senha</a></li>
                     <li>
                         <hr class="dropdown-divider"/>
                     </li>
                     <li>
-                        <a class="dropdown-item"
-                           onclick="event.preventDefault();
+                        <a class="dropdown-item" onclick="event.preventDefault();
                                         document.getElementById('logout-form').submit();">
                             Sair
                         </a>
@@ -89,36 +247,103 @@
         <nav class="sb-sidenav accordion sb-sidenav-dark" id="sidenavAccordion">
             <div class="sb-sidenav-menu">
                 <div class="nav">
+                    @if(auth()->check() && auth()->user()->user_type === 'C')
                     <div class="sb-sidenav-menu-heading">Espaço de Compras</div>
-                        <a class="nav-link" href="/catalogo">
+
+                    <a class="nav-link {{ Route::currentRouteName() == 'catalogo.index' ? 'active' : '' }}"
+                       href="{{ route('catalogo.index') }}">
+                        <div class="sb-nav-link-icon"><i class="fas fa-table"></i></div>
+                        Catálogo
+                    </a>
+
+                    <a class="nav-link {{ Route::currentRouteName() == 'catalogo.camisola' ? 'active' : '' }}"
+                       href="{{ route('catalogo.camisola') }}">
+                        <div class="sb-nav-link-icon"><i class="fas fa-graduation-cap"></i></div>
+                        Camisolas
+                    </a>
+
+                    <a class="nav-link {{ Route::currentRouteName() == 'cart.show' ? 'active' : '' }}"
+                       href="{{ route('cart.show') }}">
+                        <div class="sb-nav-link-icon"><i class="fas fa-shopping-cart"></i></div>
+                        Carrinho
+                    </a>
+
+                    @elseif(auth()->check() && auth()->user()->user_type === 'A')
+
+                        <div class="sb-sidenav-menu-heading">Espaço de Compras</div>
+
+                        <a class="nav-link {{ Route::currentRouteName() == 'catalogo.index' ? 'active' : '' }}"
+                           href="{{ route('catalogo.index') }}">
                             <div class="sb-nav-link-icon"><i class="fas fa-table"></i></div>
                             Catálogo
                         </a>
 
-                        <a class="nav-link" href="/camisolas">
+                        <a class="nav-link {{ Route::currentRouteName() == 'catalogo.camisola' ? 'active' : '' }}"
+                           href="{{ route('catalogo.camisola') }}">
                             <div class="sb-nav-link-icon"><i class="fas fa-graduation-cap"></i></div>
                             Camisolas
                         </a>
 
-{{--                    @if(Auth::user()->user_type === 'A') //CHANGE--}}
-                        <div class="sb-sidenav-menu-heading">Espaço Admin</div>
+                        <a class="nav-link {{ Route::currentRouteName() == 'cart.show' ? 'active' : '' }}"
+                           href="{{ route('cart.show') }}">
+                            <div class="sb-nav-link-icon"><i class="fas fa-shopping-cart"></i></div>
+                            Carrinho
+                        </a>
+
+                        <!-- Your dashboard content for user type A -->
+                        <div class="sb-sidenav-menu-heading">Administração</div>
                         <a class="nav-link" href="/dashboard">
                             <div class="sb-nav-link-icon"><i class="fa fa-tachometer" aria-hidden="true"></i></div>
-                            Admin Dashboard
+                            Dashboard
                         </a>
-{{--                    @endif--}}
+
+                    @elseif(auth()->check() && auth()->user()->user_type === 'E')
+
+                        <div class="sb-sidenav-menu-heading">Espaço de Compras</div>
+
+                        <a class="nav-link {{ Route::currentRouteName() == 'catalogo.index' ? 'active' : '' }}"
+                           href="{{ route('catalogo.index') }}">
+                            <div class="sb-nav-link-icon"><i class="fas fa-table"></i></div>
+                            Catálogo
+                        </a>
+
+                        <a class="nav-link {{ Route::currentRouteName() == 'catalogo.camisola' ? 'active' : '' }}"
+                           href="{{ route('catalogo.camisola') }}">
+                            <div class="sb-nav-link-icon"><i class="fas fa-graduation-cap"></i></div>
+                            Camisolas
+                        </a>
+
+                        <a class="nav-link {{ Route::currentRouteName() == 'cart.show' ? 'active' : '' }}"
+                           href="{{ route('cart.show') }}">
+                            <div class="sb-nav-link-icon"><i class="fas fa-shopping-cart"></i></div>
+                            Carrinho
+                        </a>
+
+                        <!-- Your dashboard content for user type A -->
+                        <div class="sb-sidenav-menu-heading">Administração</div>
+                        <a class="nav-link" href="/dashboard">
+                            <div class="sb-nav-link-icon"><i class="fa fa-tachometer" aria-hidden="true"></i></div>
+                            Dashboard
+                        </a>
+                    @endif
                 </div>
             </div>
-{{--            <div class="sb-sidenav-footer">--}}
-{{--                <div class="small">Logged in as: </div>--}}
-{{--                {{ Auth::user()->name }}--}}
-{{--            </div>--}}
+            {{--            <div class="sb-sidenav-footer">--}}
+            {{--                <div class="small">Logged in as: </div>--}}
+            {{--                {{ Auth::user()->name }}--}}
+            {{--            </div>--}}
         </nav>
     </div>
     <div id="layoutSidenav_content">
         <main>
             <div class="container-fluid px-4">
-                <br>
+                @if (session('alert-msg'))
+                    @include('shared.messages')
+                @endif
+                @if ($errors->any())
+                    @include('shared.alertValidation')
+                @endif
+                <h1 class="mt-4">@yield('titulo', 'ImagineShirt')</h1>
                 @yield('subtitulo')
                 <div class="mt-4">
                     @yield('main')
@@ -128,7 +353,7 @@
         <footer class="py-2 bg-light mt-auto">
             <div class="container-fluid px-4">
                 <div class="d-flex align-items-center justify-content-between small">
-                    <div class="text-muted">Copyright &copy;ImagineShirt</div>
+                    <div class="text-muted">Copyright &copy;ImagineShirt 2023</div>
                 </div>
             </div>
         </footer>
