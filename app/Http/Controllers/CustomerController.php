5547ba2cfd6f50a8fc9b7a7014e7fb4aab13e4fb
ruiguitos <ruiguitos@hotmail.com>
2023-06-21 20:50:52 +0100

    updated controller

--- app/Http/Controllers/CustomerController.php
+++ app/Http/Controllers/CustomerController.php
@@ -33,28 +33,4 @@ class CustomerController extends Controller
         return view('perfil.index', compact('customers'));
     }
 
-
-    //    public function indexAdmins() :View
-//    {
-////        $customers = Customer::select('id', 'nif', 'address', 'default_payment_type', 'default_payment_ref')->paginate(20);
-//        $users = User::select('id', 'name', 'email', 'user_type', 'blocked', 'photo_url')->paginate(20);
-//
-//        return view('users.admins.index', compact( 'users'));
-//    }
-
-//    public function indexClientes() :View
-//    {
-////        $customers = Customer::select('id', 'nif', 'address', 'default_payment_type', 'default_payment_ref')->paginate(20);
-//        $users = User::select('id', 'name', 'email', 'user_type', 'blocked', 'photo_url')->paginate(20);
-//
-//        return view('users.clientes.index', compact( 'users'));
-//    }
-
-//    public function indexEmpregados() :View
-//    {
-////        $customers = Customer::select('id', 'nif', 'address', 'default_payment_type', 'default_payment_ref')->paginate(20);
-//        $users = User::select('id', 'name', 'email', 'user_type', 'blocked', 'photo_url')->paginate(20);
-//
-//        return view('users.empregados.index', compact( 'users'));
-//    }
 }
